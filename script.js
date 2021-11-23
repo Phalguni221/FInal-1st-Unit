0fdde30fa34c6043aefabf70fd43b99080d8eb57
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2021-11-22 21:48:53 -0500

    FINAL-1ST-UNIT

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-document.getElementById("Click").addEventListener("click", displayDate);
+document.getElementById("myBtn").addEventListener("click", displayDate);
 
 function displayDate() {
-    document.getElementById("demo").innerHTML = Date();
-  }
+  document.getElementById("demo").innerHTML = Date();
+}
