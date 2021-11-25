554cab6b02dd2af81c13f142eaf9caa616e91023
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2021-11-24 19:54:51 -0500

    FINAL-1ST-UNIT

--- game.js
+++ game.js
@@ -1,28 +1,66 @@
-function newPlayableCharacter(x, y) {
-    const element = newImage('assets/green-character/static.gif')
-    element.style.zIndex = 1;
+// let modifier = 5;
+// document.addEventListener('keydown', (event)=> {
+//     const {style} = document.querySelector(".PrincessPeach");
+//         if(event.repeat) return;
 
-    function handleDirectionChange(direction) {
-        if (direction === null) {
-            element.src = `assets/green-character/static.gif`
-        }
-        if (direction === 'west') {
-            element.src = `assets/green-character/west.gif`
-        }
-        if (direction === 'north') {
-            element.src = `assets/green-character/north.gif`
-        }
-        if (direction === 'east') {
-            element.src = `assets/green-character/east.gif`
-        }
-        if (direction === 'south') {
-            element.src = `assets/green-character/south.gif`
-        }
+//     if(event.key === 'ArrowLeft'){
+//         direction = 'west'
+//     }
+//     if(event.key === 'ArrowUp'){
+//         direction = 'north'
+//     }
+//     if(event.key === 'ArrowRight'){
+//         direction = 'east'
+//     }
+//     if(event.key === 'ArrowDown'){
+//         direction = 'south'
+//     }
+//     callback(direction)
+// })
+//     if(event.repeat) return;
+
+    let modifier = 5;
+    document.addEventListener('keydown', (event)=> {
+    const {style} = document.querySelector(".PrincessPeach");
+    switch(event.key) {
+        case 'ArrowUp': style.top = `${parseInt(style.top)-modifier}px`;break;
+         case 'ArrowDown': style.top = `${parseInt(style.top)+modifier}px`;break;
+        case 'ArrowLeft': style.left = `${parseInt(style.top)-modifier}px`;break;
+        case 'ArrowRight': style.left = `${parseInt(style.top)+modifier}px`;break;
     }
+    console.log(event.key);
+});
 
-    move(element).withArrowKeys(x, y, handleDirectionChange)
+// document.addEventListener('keydown', function(e){
+//     if(e.repeat) return;
 
-    return {
-        element: element
-    }
-}
+//     if(e.key === 'ArrowLeft'){
+//         direction = 'west'
+//     }
+//     if(e.key === 'ArrowUp'){
+//         direction = 'north'
+//     }
+//     if(e.key === 'ArrowRight'){
+//         direction = 'east'
+//     }
+//     if(e.key === 'ArrowDown'){
+//         direction = 'south'
+//     }
+//     callback(direction)
+// })
+// function tile (url, top, bottom, width, height){
+//     for(let h = 0; h < height; h++){
+//         for(let w = 0; w < width; w++){
+//             newImage(url, top + w*100, bottom+h*100)
+//         }
+//     }
+// }
+
+// let GrassHeight = window.innerHeight / 2
+// // let SkyHeight = window.innerHeight-GrassHeight
+
+// // tile('assets/sky.png', 0, 500, window.innerWidth/100, SkyHeight/100)
+// tile('assets/grass.png', 0, 0, window.innerWidth/100, GrassHeight/100)
+
+// // newImage('.assets/sky.png',0,0)
+// newImage('.assets/grass.png', 0, 0)
