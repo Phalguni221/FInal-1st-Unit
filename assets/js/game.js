a2e26052c152f4639be7e68fee76fbac1abf933d
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2021-11-28 17:38:07 -0500

    FINAL-1ST-UNIT

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,31 +1,18 @@
 
-    let modifier = 50;
-    document.addEventListener('keydown', (event)=> {
-    let princess = document.querySelector(".PrincessPeach");
-    let cloud = 0
-
-    princess.style.left = "0px"
-    princess.style.top = "0px"
-   
-    switch(event.key) {
-        case "ArrowUp": princess.style.top = `${parseInt(princess.style.top)-modifier-cloud}px`;break;
-         case "ArrowDown": princess.style.top = `${parseInt(princess.style.top)+modifier+cloud}px`;break;
-        case "ArrowLeft": princess.style.left = `${parseInt(princess.style.left)-modifier-cloud}px`;break;
-        case "ArrowRight": princess.style.left = `${parseInt(princess.style.left)+modifier+cloud}px`;break;
-    }
-    // console.log(event.key);
-});
 
 const grid = document.querySelector('.grid')
+const DisplayResults = document.querySelector('.results')
 let currentCollectorIndex = 50
 let width = 15
 let height= 30
 
-let direction =1
+let direction = 1
 let cupcakesId = 1
+let goingRight = true
 
 
-for(let i = 0; i < 60; i++) {
+
+for(let i = 0; 0< i < 100; i++) {
     const square = document.createElement('div')
     grid.appendChild(square)
 }
@@ -45,7 +32,7 @@ function draw () {
     }
 }
 
-draw()
+
 
 function remove () {
     for (let i = 0; i<cupcakes.length; i++) {
@@ -55,12 +42,12 @@ function remove () {
 
 squares[currentCollectorIndex].classList.add('picture')
 
-function moveCollector(e) {
+function movePicture(e) {
     squares[currentCollectorIndex].classList.remove('picture')
     switch(e.key) {
-        case "ArrowUp": if (parseInt(currentCollectorIndex)-modifier) currentCollectorIndex +=1
+        case "ArrowUp": if (currentCollectorIndex % height < height-1) currentCollectorIndex -=1
         break
-         case "ArrowDown":if (currentCollectorIndex % height < height-1) currentCollectorIndex -=1
+         case "ArrowDown":if (currentCollectorIndex % height < height-1) currentCollectorIndex +=1
          break
         case "ArrowLeft": if (currentCollectorIndex % width !==0) currentCollectorIndex -=1
         break
@@ -72,18 +59,25 @@ squares[currentCollectorIndex].classList.remove('picture')
 
 document.addEventListener('keydown', moveCollector)
 
-
-
 function moveCupcakes() {
     const leftEdge = cupcakes[0] % width === 0
     const rightEdge = cupcakes[cupcakes.length - 1] % width === width-1
     remove()
 }
 
-if (rightEdge) {
+if (rightEdge && goingRight) {
     for (let i=0; i < cupcakes.length; i++) {
-        cupcakes[i] += width -1
+        cupcakes[i] += width +1
         direction = -1
+        goingRight = false
+    }
+}
+
+if(leftEdge && !goingRight) {
+    for(let i =0; i<cupcakes.length;i++) {
+        cupcakes[i] += width -1
+        direction = 1
+        goingRight = true
     }
 }
 
@@ -92,4 +86,17 @@ for (let i=0; i < cupcakes.length; i++) {
 }
 draw()
 
-cupcakesId = setInterval(moveCupcakes, 500)
+if (squares[currentCollectorIndex].classList.contains('cupcake', 'picture')) {
+    DisplayResults.innerHTML("3 points!")
+    // clearInterval(cupcakesId)
+}
+
+for(let i=0; i <cupcakes.length; i++) {
+    if(cupcakes[i] > squares.length) {
+        let text = "Game Over! (Click Here to view your Scores!)";
+let result = text.link("scoreboard.html");
+    }
+}
+
+cupcakesId = setInterval(moveCupcakes,500)
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,17 @@
-document.getElementById("myBtn").addEventListener("click", displayDate);
 
-function displayDate() {
-  document.getElementById("demo").innerHTML = Date();
-}
+    let modifier = 50;
+    document.addEventListener('keydown', (event)=> {
+    let princess = document.querySelector(".PrincessPeach");
+    let cloud = 0
+
+    princess.style.left = "0px"
+    princess.style.top = "0px"
+   
+    switch(event.key) {
+        case "ArrowUp": princess.style.top = `${parseInt(princess.style.top)-modifier-cloud}px`;break;
+         case "ArrowDown": princess.style.top = `${parseInt(princess.style.top)+modifier+cloud}px`;break;
+        case "ArrowLeft": princess.style.left = `${parseInt(princess.style.left)-modifier-cloud}px`;break;
+        case "ArrowRight": princess.style.left = `${parseInt(princess.style.left)+modifier+cloud}px`;break;
+    }
+    // console.log(event.key);
+});
