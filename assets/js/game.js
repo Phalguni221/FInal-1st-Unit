689e35aa1e855e369214cd3bde03cfe1845f5b15
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2021-11-30 12:56:26 -0500

    FINAL-1ST-UNIT

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,4 @@
-
+//set Princess Peach movements
 let modifier = 50;
 document.addEventListener('keydown', (event) => {
     let princess = document.querySelector(".PrincessPeach");
@@ -13,9 +13,10 @@ document.addEventListener('keydown', (event) => {
         case "ArrowLeft": princess.style.left = `${parseInt(princess.style.left) - modifier - cloud}px`; break;
         case "ArrowRight": princess.style.left = `${parseInt(princess.style.left) + modifier + cloud}px`; break;
     }
-    // console.log(event.key);
+    console.log(event.key);
 });
 
+//Set the grid for the cupcakes falling from the sky
 const grid = document.querySelector('.grid')
 const DisplayResults = document.querySelector('.results')
 let currentCollectorIndex = 50
@@ -28,38 +29,34 @@ let goingRight = true
 
 
 
-for (let i = 0; 0 < i < 100; i++) {
-    const square = document.createElement('div')
-    grid.appendChild(square)
+for (let i = 0;  i < 70; i++) {
+    const squares = document.createElement('div')
+    grid.appendChild(squares)
 }
 
 const squares = grid.children
 
-const cupcakes = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-    21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-    31, 32, 33
-]
 
+//Add and remove the cupcakes
 function draw() {
-    for (let i = 0; i < cupcakes.length; i++) {
+    for (let i = 0; i < cupcake.length; i++) {
         squares[cupcakes[i]].classList.add('cupcake')
     }
 }
 
 
-//Rename to "RemoveCupcake"
 function removeCupcake() {
     for (let i = 0; i < cupcakes.length; i++) {
         squares[cupcakes[i]].classList.remove('cupcake')
     }
+    
 }
 
-squares[currentCollectorIndex].classList.add('picture')
+squares[currentCollectorIndex].classList.add('PrincessPeach')
 
+//
 function movePicture(e) {
-    squares[currentCollectorIndex].classList.remove('picture')
+    squares[currentCollectorIndex].classList.remove('PrincessPeach')
     switch (e.key) {
         case "ArrowUp": if (currentCollectorIndex % height < height - 1) currentCollectorIndex -= 1
             break
@@ -70,49 +67,48 @@ function movePicture(e) {
         case "ArrowRight": if (currentCollectorIndex % width < width - 1) currentCollectorIndex += 1
             break
     }
-    squares[currentCollectorIndex].classList.remove('picture')
+    squares[currentCollectorIndex].classList.remove('PrincessPeach')
 }
 
-// document.addEventListener('keydown', movePicture)
+document.addEventListener('keydown', movePicture)
 
 function moveCupcakes() {
     const leftEdge = cupcakes[0] % width === 0
     const rightEdge = cupcakes[cupcakes.length - 1] % width === width - 1
-    removeCupcake()
 }
 
-if (rightEdge && goingRight) {
-    for (let i = 0; i < cupcakes.length; i++) {
-        cupcakes[i] += width + 1
-        direction = -1
-        goingRight = false
-    }
-}
+// if (rightEdge && goingRight) {
+//     for (let i = 0; i < cupcakes.length; i++) {
+//         cupcakes[i] += width + 1
+//         direction = -1
+//         goingRight = false
+//     }
+// }
 
-if (leftEdge && !goingRight) {
-    for (let i = 0; i < cupcakes.length; i++) {
-        cupcakes[i] += width - 1
-        direction = 1
-        goingRight = true
-    }
-}
+// if (leftEdge && !goingRight) {
+//     for (let i = 0; i < cupcakes.length; i++) {
+//         cupcakes[i] += width - 1
+//         direction = 1
+//         goingRight = true
+//     }
+// }
 
-for (let i = 0; i < cupcakes.length; i++) {
-    cupcakes[i] += direction
-}
-// draw()
+// for (let i = 0; i < cupcakes.length; i++) {
+//     cupcakes[i] += direction
+// }
+// // draw()
 
-if (squares[currentCollectorIndex].classList.contains('cupcake', 'picture')) {
-    DisplayResults.innerHTML("3 points!")
-    // clearInterval(cupcakesId)
-}
+// if (squares[currentCollectorIndex].classList.contains('cupcake', 'picture')) {
+//     DisplayResults.innerHTML("3 points!")
+//     // clearInterval(cupcakesId)
+// }
 
-for (let i = 0; i < cupcakes.length; i++) {
-    if (cupcakes[i] > squares.length) {
-        let text = "Game Over! (Click Here to view your Scores!)";
-        let result = text.link("scoreboard.html");
-    }
-}
+// for (let i = 0; i < cupcakes.length; i++) {
+//     if (cupcakes[i] > squares.length) {
+//         let text = "Game Over! (Click Here to view your Scores!)";
+//         let result = text.link("scoreboard.html");
+//     }
+// }
 
 // setInterval(moveCupcakes, 500)
 
