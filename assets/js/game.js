428008e0cec8cefb1f9914a51136de7c7f15f02f
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2021-12-03 22:29:52 -0500

    FINAL-1ST-UNIT

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,48 +3,111 @@
 //Results Setup
 const DisplayResults = document.querySelector('.results')
 
-//Falling Cupcake Grid Setup
+
+//CloudMovement
+// var cloudImage = null;
+// var cloudVelocity = 1;
+// var cloudAnimation;
+
+// var cloudImage2 = null;
+// var cloudVelocity2 = 1;
+// var cloudAnimation2;
+
+// function init() {
+//     cloudImage= document.getElementById('cloudImage');
+//     cloudImage.style.left = 0;
+//  cloudAnimation = setInterval(leftcloudMovement,1000);
+// }
+// function leftcloudMovement() {
+//     if (cloudImage.style.left > 50 &&  cloudVelocity < 0) {
+//         cloudVelocity = -cloudVelocity;
+//     }
+//     cloudImage.style.left = parseInt(cloudImage.style.left) + cloudVelocity * 100 + 'px';
+// }
+
+// init()
 
 const grid = document.querySelector('.grid');
-
-
-
+ 
 setInterval(draw => {
-    for (let i = 0;  i < 1;  i++) {
+    for (let i = 0;  i<1;  i++) {
         const squares = document.createElement('div');
         grid.appendChild(squares);
     }
     
-    },5000)
+    },1000)
 
-        // if (Math.random() < 0.1);
-        // var square = new Object();
-        // square.x = Math.floor(Math.random()*600);
-        // square.y=5;
+ const squares = grid.children
 
- const square = grid.children
-    
+ var myGameArea = {
+    div : document.createElement("div"),
+    start : function() {
+        this.div.width = 40;
+        this.div.height = 40;
+        this.context = this.div.getContext("2d");
+        document.body.insertBefore(this.div, document.body.childNodes[0]);
+        this.frameNo = 0;
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    stop : function() {
+        clearInterval(this.interval);
+    },    
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+
+function component(width, height, color, x, y, type) {
+
+    this.type = type;
+    this.width = width;
+    this.height = height;
+    this.speed = 1;
+    this.angle = 0;
+    this.x = x;
+    this.y = y;    
+    this.update = function() {
+        ctx = myGameArea.context;
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle);
+        ctx.fillStyle = color;
+        ctx.fillRect(this.width / -2, this.height / -2, this.width, this.height);        
+        ctx.restore();    
+    }
+    this.newPos = function() {
+        this.x += this.speed * Math.sin(this.angle);
+        this.y -= this.speed * Math.cos(this.angle);
+    }
+}
+
+function updateGameArea() {
+    myGameArea.clear();
+    myGamePiece.newPos();
+    myGamePiece.update();
+}
 
 
-//set Princess Peach movements
+//PrincessPeachMovements
+
 let modifier = 200;
 document.addEventListener('keydown', (event) => {
-    let princess = document.querySelector(".PrincessPeach");
+    let princess = document.querySelector(".PrincessPeachCute");
     princess.style.left = "0px"
     princess.style.top = "0px"
 
     switch (event.key) {
-        case "ArrowUp": princess.style.top = `${parseInt(princess.style.top) - modifier}px`; break;
-        case "ArrowDown": princess.style.top = `${parseInt(princess.style.top) + modifier}px`; break;
-        case "ArrowLeft": princess.style.left = `${parseInt(princess.style.left) - modifier}px`; break;
-        case "ArrowRight": princess.style.left = `${parseInt(princess.style.left) + modifier}px`; break;
+        case "ArrowUp": princess.style.top = `${parseInt(princess.style.top)-modifier}px`; break;
+        case "ArrowDown": princess.style.top = `${parseInt(princess.style.top)+modifier}px`; break;
+        case "ArrowLeft": princess.style.left = `${parseInt(princess.style.left)-modifier}px`; break;
+        case "ArrowRight": princess.style.left = `${parseInt(princess.style.left)+modifier}px`; break;
     }
     console.log(event.key);
 });
 
 
 function movePicture(e) {
-    squares[currentCollectorIndex].classList.remove('PrincessPeach')
+    squares[currentCollectorIndex].classList.remove('PrincessPeachCute')
     switch (e.key) {
         case "ArrowUp": if (currentCollectorIndex % height < height - 1) currentCollectorIndex -= 1
             break
@@ -55,40 +118,44 @@ function movePicture(e) {
         case "ArrowRight": if (currentCollectorIndex % width < width - 1) currentCollectorIndex += 1
             break
     }
-    squares[currentCollectorIndex].classList.remove('PrincessPeach')
+    squares[currentCollectorIndex].classList.remove('PrincessPeachCute')
 }
 
 document.addEventListener('keydown', movePicture)
 
-// // let goUp = "./assets/images/PrincessPeachCute.jpg";
-// // let goLeft = "./assets/images/PrincessPeachCute.jpg";
-// // let goDown = "./assets/images/PrincessPeachCute.jpg";
-// // let goUp = "./assets/images/PrincessPeachCute.jpg";
+// let goLeft = "/assets/images/PrincessPeachCute.jpg";
+// let goUp = "/assets/images/PrincessPeachCute.jpg";
+// let goRight = "./assets/images/PrincessPeachCute.jpg";
+// let goDown = "./assets/images/PrincessPeachCute.jpg";
 
-// // let moves = {
-// //     directionChanges: {
-// //         // Move with arrow keys
-// //         "ArrowLeft": {  // West
-// //             // x: -1, y: 0,
-// //             // backgroundImage: "./assets/images/PrincessPeachCute.jpg",
-// //             // PrincessPeachImg: goLeft,
-// //         },
-// //         "ArrowUp": {  // North
-// //             x: 0, y: 1,
-// //             backgroundImage: "./assets/images/PrincessPeachCute.jpg",  // TODO: Want a different one for each direction
-// //             PrincessPeachImg: goUp,
-// //         },
-// //         "ArrowRight": {  // East
-// //             x: 1, y: 0,
-// //             backgroundImage: "./assets/images/PrincessPeachCute.jpg",
-// //             PrincessPeachImg: goRight,
-// //         },
-// //         "ArrowDown": {  // South
-// //             x: 0, y: -1,
-// //             backgroundImage: "./assets/images/PrincessPeachCute.jpg",  // TODO: Want a different one for each direction
-// //             PrincessPeachImg: goDown,
-// //         },
-// //         // TODO: Allow WASD movement, too?
-// //     },
+// let moves = {
+//     goLeft, //Encapsulate goLeft,
+//         goUp, //Encapsulate goUp,
+//         goRight, //Encapsulate goRight,
+//         goDown, //Encapsulate goDown
+//         // Move with arrow keys
+//     directionChanges: {
+//         "ArrowLeft": {  // West
+//             x: -1, y: 0,
+//             backgroundImage: "./assets/images/PrincessPeachCute.jpg",
+//             PrincessPeachImg: goLeft,
+//         },
+//         "ArrowUp": {  // North
+//             x: 0, y: 1,
+//             backgroundImage: "./assets/images/PrincessPeachCute.jpg",  // TODO: Want a different one for each direction
+//             PrincessPeachImg: goUp,
+//         },
+//         "ArrowRight": {  // East
+//             x: 1, y: 0,
+//             backgroundImage: "./assets/images/PrincessPeachCute.jpg",
+//             PrincessPeachImg: goRight,
+//         },
+//         "ArrowDown": {  // South
+//             x: 0, y: -1,
+//             backgroundImage: "./assets/images/PrincessPeachCute.jpg",  // TODO: Want a different one for each direction
+//             PrincessPeachImg: goDown,
+//         },
+//         // TODO: Allow WASD movement, too?
+//     },
 
-
+// }
--- assets/js/timerCountDown.js
+++ assets/js/timerCountDown.js
@@ -6,47 +6,47 @@ let currentTime = new Date();
 // console.log(cMonth);
 // console.log(cYear);
 
-// Set the Time we're counting down to
-var countDownDate = new Date( "Dec 30 2021 00:00").getTime();
+// mytime = setTimeout(expression, msec);
+// // Set the Time we're counting down to
+// var countDownDate = new Date( "Dec 30 2021 00:00").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+// // Update the count down every 1 second
+// var x = setInterval(function() {
 
-  // Get today's date and time
-  var now = new Date().getTime();
+//   // Get today's date and time
+//   var now = new Date().getTime();
     
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+//   // Find the distance between now and the count down date
+//   var distance = countDownDate - now;
     
-  // Time calculations for days, hours, minutes and seconds
+//   // Time calculations for days, hours, minutes and seconds
  
-  var minutes = Math.floor((distance % (1000 * 120)) / 1000);
-  var seconds = Math.floor((distance % (1000 * 120)) / 1000);
+//   var minutes = Math.floor((distance % (1000 * 120)) / 1000);
+//   var seconds = Math.floor((distance % (1000 * 120)) / 1000);
     
-  // Output the result in an element with id="demo"
-  document.getElementById("timer").innerHTML = seconds + "s ";
+//   // Output the result in an element with id="demo"
+//   document.getElementById("timer").innerHTML = seconds + "s ";
     
-  // If the count down is over, write some text 
-  if (distance === 0) {
-    clearInterval(x);
-    document.getElementById("timer").innerHTML = "Time's Up!";
+//   // If the count down is over, write some text 
+//   if (distance = 0) {
+//     clearInterval(x);
+//     document.getElementById("timer").innerHTML = "Time's Up!";
+//   };
+
+//   })
+  console.clear()
+  let timer = document.querySelector('h1')
+  let startingTime = 120
+  let startTimer;
+  let startStopTimer = () => {
+    if(startTimer){
+      clearInterval(startTimer);
+      timer.innerHTML = '02:00'
+      startingTime = 120
+      return startTimer = undefined
+    }else{
+      return startTimer = setInterval(()=>{
+        timer.innerHTML = `00:0${startingTime--}`
+      }, 1000)
+    }
   }
-
-  // else (distance < 120)
-
-  //   var x = setInterval(function() {
-
-  //     // Get today's date and time
-  //     var now = new Date().getTime();
-        
-  //     // Find the distance between now and the count down date
-  //     var distance = countDownDate - now;
-        
-  //     // Time calculations for days, hours, minutes and seconds
-  //     var seconds = Math.floor((distance % (1000 * 120)) / 1000);
-        
-  //     // Output the result in an element with id="demo"
-  //     document.getElementById("timer").innerHTML = seconds + "s ";
-  // });
-
-  })
