7ee053b9b28a6a307508efacf546e55bad9c5e8f
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2021-12-06 20:46:20 -0500

    FINAL-1St-UNIT

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,10 +3,6 @@
 //Results Setup
 const DisplayResults = document.querySelector('.results')
 
-// let c= squares[i]
-// let princess = document.querySelector(".PrincessPeachCute");
-// let b = collisionDetection;
-
 function getRandomInt() {
     return Math.floor(Math.random() * 800);
 }
@@ -29,7 +25,7 @@ setInterval(() => {
 //Remove cupcakes when they touch ground
 setInterval(() => {
     for (let i = 0; i < squares.length; i++) {
-        let c = squares[i];
+       let c= squares[i];
         console.log("Did it hit the ground?", c)
         // if cupcake is at height of ground, then remove it 
         // if cupcake(vertical position) >= ground(vertical position) then remove cupcake 
@@ -40,76 +36,18 @@ setInterval(() => {
     }
 }, 500)
 
-// isCollision(x, y, remove = true) {
-//     let collision = this.board[x][y] === remove ? 1 : 0
-//     if (collision === 1 && remove === true) {
-//         this.removeFromBoard(x, y);
-
-//         // tail.addToTail(x, y) // TODO?
-//     }
-//     return collision;
-// },
-
-
-
-
-// function toggle_visibility(id) {
-//     var c = document.getElementsByClassName("div");
-//     if(square.style.top == `${500}px`)
-//     square.style.top = 'none';
-//     else
-//     square.style.top = `${x}px`;
-//  }
-
-
-// function collisionDetection() {
-//     for(let c ; c< squares.length; c++) {
-//         for(let princess; princess<squares.length; princess++) {
-//             var b = squares[c][princess];
-//             // calculations
-//         }
-//     }
-// }
-
-// function checkGround() {
-//     for(let i=0; i<1; i++) {
-// console.log(square[i])
-
-//     }
-// }
-
-//games.js
-
-// //create cupcakes once -> list
-// squares = [] 
-// ​
-// //creating cupcakes
-// setInterval(() => {
-//     //createElement...
-// },1000)
-// ​
-// //checking for ground collision
-// setInterval (() => {
-//     for (let index = 0; index < squares.length; index++) {
-//         let c = squares[index];
-//         //where on screen?
-//         //remove if top > 1000 (whever the ground is)
-//     }
-// }, 500)
-
-//
-//Stop function draw() at a height
-
 //PrincessPeachMovements
 
 let modifier = 7;
 let princess = document.querySelector(".PrincessPeachCute");
+let princessCoord = {x:0,y:0, width:20, height:20}
 princess.style.top = "0px";
 princess.style.left = "0px";
 document.addEventListener('keydown', (event) => {
     console.log(`${parseInt(princess.style.top.replace('px', "")) - modifier}px`)
     switch (event.key) {
-        case "ArrowUp": princess.style.top = `${parseInt(princess.style.top) - modifier}px`; break;
+        case "ArrowUp": princess.style.top = `${parseInt(princess.style.top) - modifier}px`;
+        ArrowUp.y = event.clientY;break;
         case "ArrowDown": princess.style.top = `${parseInt(princess.style.top) + modifier}px`; break;
         case "ArrowLeft": princess.style.left = `${parseInt(princess.style.left) - modifier}px`; break;
         case "ArrowRight": princess.style.left = `${parseInt(princess.style.left) + modifier}px`; break;
@@ -118,12 +56,15 @@ document.addEventListener('keydown', (event) => {
     console.log(event.key);
 });
 
+//Collision of Objects 
+if(squareCoord.x < princessCoord.x +princessCoord.width &&
+    squareCoord.x +squareCoord.width > princessCoord.x &&
+    squareCoord.y < princessCoord.y + princessCoord.height &&
+    squareCoord.y + squareCoord.height > princessCoord.y 
+    ){
+        visibility:hidden;
+    } else {
+        //no collision
+    }
 
-
-// function collisionDetection() {
-//     for(var c=0; c<brickColumnCount; c++) {
-//         for(var r=0; r<brickRowCount; r++) {
-//             var b = bricks[c][r];
-//             // calculations
-//         }
-//     }
+//Get Score on Collision
