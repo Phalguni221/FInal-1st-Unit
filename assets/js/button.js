8d48b42a3bf6f97cd3397c3f908d463f0fa66252
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2023-05-13 18:18:09 -0500

    Update button.js

--- assets/js/button.js
+++ assets/js/button.js
@@ -1,13 +1,28 @@
+
 <input type="button" id="Button" onclick="Buttontoggle()"></input>
 
-function Buttontoggle()
+function Buttontoggle() {
 
-{
-  var t = document.getElementById('playButton');
-  if(t.value=="Play"){
-      t.value="Pause";}
+// {
+//   var t = document.getElementById('playButton');
+//   if(t.value=="Play"){
+//       t.value="Pause";}
 
-  else if(t.value=="Pause"){
-      t.value="Play";}
+//   else if(t.value=="Pause"){
+//       t.value="Play";}
+// }
+
+
+//play-pause buttons 
+function playPause() { 
+
+    let PlayPause = document.getElementById('playButton');
+    
+    if (PlayPause.paused) 
+      PlayPause.play();
+      
+    if
+      (PlayPause.play)
+    PlayPause.pause();  
+    }
 }
-
