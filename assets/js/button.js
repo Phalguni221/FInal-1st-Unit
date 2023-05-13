3909849e3a044cf6b8a23c93e5f3a7f5cb652db6
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2023-05-13 17:41:51 -0500

    Update button.js

--- assets/js/button.js
+++ assets/js/button.js
@@ -1,24 +1,13 @@
-{/* <button id="playButton" type="button" class="btn btn-default btn-xs">
-Play
-<span class="glyphicon glyphicon-pause"></span>
-<!-- <span class="glyphicon glyphicon-play"></span> -->
-</button>
-<button id="pauseButton" type="button" class="btn btn-default btn-xs">
-Pause
-<span class="glyphicon glyphicon-pause"></span>
-</button> */}
+<input type="button" id="Button" onclick="Buttontoggle()"></input>
 
-
-<input type="button" id="Button"
-       onclick="Buttontoggle();">
-       </input>
 function Buttontoggle()
 
 {
-  var t = document.getElementById("Button");
+  var t = document.getElementById('playButton');
   if(t.value=="Play"){
       t.value="Pause";}
 
   else if(t.value=="Pause"){
       t.value="Play";}
 }
+
